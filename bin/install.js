8c65243a5bf1d08cfa7ed46dee30d906a4d1cdb9
Daniel Jackson <daniel@cloakedninjas.co.uk>
2016-08-18 13:08:22 +0900

    Updating to latest Phaser version - install script removes our deps - prettier output

--- bin/install.js
+++ bin/install.js
@@ -1,19 +1,49 @@
 #!/usr/bin/env node
 
-var exec = require('child_process').exec,
-    fs = require('fs');
+var fs = require('fs'),
+    util = require('util'),
+    spawn = require('child_process').spawn,
+    npmInstall = spawn('npm', ['install']);
 
-console.log('Installing npm dependencies...');
+console.log(styleText('Beginning Installtion Process'));
+console.log('=============================');
+console.log(styleText('Installing npm dependencies...'));
 
-exec('npm install', function (error) {
-  if (!error) {
-    console.log('Installing bower dependencies...');
-    exec('bower install', function (error) {
-      if (!error) {
+npmInstall.stdout.on('data', function (data) {
+  pipeOutput(data);
+});
+
+npmInstall.stderr.on('data', function (data) {
+  pipeOutput(data);
+});
+
+npmInstall.on('exit', function (code) {
+  if (code === 0) {
+    console.log(styleText('Installing bower dependencies...'));
+
+    var bowerInstall = spawn('bower', ['install']);
+
+    bowerInstall.stdout.on('data', function (data) {
+      pipeOutput(data);
+    });
+
+    bowerInstall.stderr.on('data', function (data) {
+      pipeOutput(data);
+    });
+
+    bowerInstall.on('exit', function (code) {
+      if (code === 0) {
         promptForNamespace();
+      }
+      else {
+        console.error(styleText('Failed to install bower dependencies', 'red'));
       }
     });
   }
+  else {
+    console.error(styleText('Failed to install npm dependencies', 'red'));
+  }
+
 });
 
 function promptForNamespace() {
@@ -26,7 +56,7 @@ function promptForNamespace() {
 
   prompt.get([{
     name: 'namespace',
-    description: 'What will your namespace be?'.green,
+    description: 'What will your namespace be?',
     type: 'string',
     required: true
   }], function (err, result) {
@@ -71,26 +101,90 @@ function promptForCleanup() {
 
   prompt.get([{
     name: 'cleanup',
-    description: 'Initialize Git? (Y / N)'.green,
+    description: 'Initialize Git? (Y / N)',
     type: 'string',
     default: 'Y',
     required: true
   }], function (err, result) {
     if (!err && (result.cleanup === 'Y' || result.cleanup === 'y')) {
-      cleanup();
+      cleanupGit();
+    }
+    else {
+      cleanupNode();
     }
   });
 }
 
-function cleanup() {
+function cleanupGit() {
   var rmrf = require('rimraf');
 
   rmrf('.git', function (error) {
     if (!error) {
-      exec('git init', function () {
-        console.log('Git initialized.');
-        console.log('Tip: You might want to adjust bower.json + package.json');
+      gitInit = spawn('git', ['init']);
+
+      gitInit.stdout.on('data', function (data) {
+        pipeOutput(data);
+      });
+
+      gitInit.stderr.on('data', function (data) {
+        pipeOutput(data);
+      });
+
+      gitInit.on('exit', function () {
+        cleanupNode();
       });
     }
   });
+}
+
+function cleanupNode() {
+  var packageJson = require('../package.json'),
+      npmRm = spawn('npm', ['rm', '-D'].concat(Object.keys(packageJson.devDependencies)));
+
+  console.log(styleText('Removing build dependencies...'));
+
+  npmRm.stdout.on('data', function (data) {
+    pipeOutput(data);
+  });
+
+  npmRm.stderr.on('data', function (data) {
+    pipeOutput(data);
+  });
+
+  npmRm.on('exit', function () {
+    cleanupBin();
+  });
+}
+
+function cleanupBin() {
+  var exec = require('child_process').exec;
+
+  exec('rm bin/* && rmdir bin/', function () {
+    showInstallSuccess();
+  });
+}
+
+function showInstallSuccess() {
+  console.log('==============================');
+  console.log(styleText('Install completed successfully') + '\n');
+  console.log('It is recommended to run: `npm init && bower init` to define your own package settings');
+  console.log('==============================');
+}
+
+function pipeOutput(data) {
+  process.stdout.write(data.toString());
+}
+
+function styleText(text, colour) {
+  if (!colour) {
+    colour = 'green';
+  }
+
+  var colours = {
+        green: 32,
+        red: 31
+      },
+      c = colours[colour];
+
+  return '\u001b[' + c + 'm' + text + '\u001b[39m';
 }
