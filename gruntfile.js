e9f553a3cd3484ed9bd4b5e514f5952fe7fe1ea7
Daniel Jackson <daniel@cloakedninjas.co.uk>
2018-07-07 21:30:48 +0100

    Updating grunt-ts + adding dist build

--- gruntfile.js
+++ gruntfile.js
@@ -1,69 +1,109 @@
 module.exports = function (grunt) {
-    grunt.initConfig({
-        ts: {
-            dev: {
-                src: ['src/scripts/**/*.ts'],
-                dest: 'public/js',
-                options: {
-                    module: 'amd', //or commonjs
-                    target: 'es5', //or es3
-                    sourceMap: false,
-                    declaration: false
-                }
-            }
-        },
-
-        copy: {
-            dev: {
-                files: [
-                    {
-                        expand: true,
-                        cwd: 'src',
-                        src: [
-                            'assets/**'
-                        ],
-                        dest: 'public/'
-                    },
-                    {
-                        src: 'src/index.html',
-                        dest: 'public/index.html'
-                    },
-                    {
-                        src: 'node_modules/phaser/build/custom/phaser-no-physics.js',
-                        dest: 'public/vendor/phaser/phaser.js'
-                    },
-                    {
-                        src: 'node_modules/phaser-ce/build/custom/phaser-no-physics.js',
-                        dest: 'public/vendor/phaser/phaser.js'
-                    }
-                ]
-            }
-        },
-
-        clean: {
-            dev: ['public/**/*']
-        },
-
-        watch: {
-            scripts: {
-                files: ['src/**/*'],
-                tasks: ['dev'],
-                options: {
-                    spawn: false,
-                    debounceDelay: 250
-                }
-            }
+  grunt.initConfig({
+    ts: {
+      dev: {
+        src: ['src/scripts/**/*.ts'],
+        dest: 'public/js',
+        options: {
+          module: 'amd', //or commonjs
+          target: 'es5', //or es3
+          sourceMap: false,
+          declaration: false
         }
-    });
+      },
+      prod: {
+        src: ['src/scripts/**/*.ts'],
+        reference: "src/refs.d.ts",
+        out: 'public/game.min.js',
+        options: {
+          sourceMap: false
+        }
+      }
+    },
 
-    grunt.loadNpmTasks('grunt-ts');
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-watch');
+    copy: {
+      dev: {
+        files: [
+          {
+            expand: true,
+            cwd: 'src',
+            src: [
+              'assets/**'
+            ],
+            dest: 'public/'
+          },
+          {
+            src: 'src/index.html',
+            dest: 'public/index.html'
+          },
+          {
+            src: 'node_modules/phaser-ce/build/custom/phaser-no-physics.js',
+            dest: 'public/vendor/phaser/phaser.js'
+          }
+        ]
+      },
+      prod: {
+        files: [
+          {
+            expand: true,
+            cwd: 'src',
+            src: [
+              'assets/**'
+            ],
+            dest: 'public/'
+          },
+          {
+            src: 'src/index-dist.html',
+            dest: 'public/index.html'
+          },
+          {
+            src: 'node_modules/phaser-ce/build/custom/phaser-no-physics.js',
+            dest: 'public/vendor/phaser/phaser.js'
+          }
+        ]
+      }
+    },
 
-    grunt.registerTask('dev', [
-        'clean:dev',
-        'ts:dev',
-        'copy:dev'
-    ]);
+    clean: {
+      dev: ['public/**/*']
+    },
+
+    watch: {
+      scripts: {
+        files: ['src/**/*'],
+        tasks: ['dev'],
+        options: {
+          spawn: false,
+          debounceDelay: 250
+        }
+      }
+    },
+
+    uglify: {
+      prod: {
+        files: {
+          'public/game.min.js': ['public/game.min.js']
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-ts');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.registerTask('dev', [
+    'clean:dev',
+    'ts:dev',
+    'copy:dev'
+  ]);
+
+  grunt.registerTask('prod', [
+    'clean:dev',
+    'ts:prod',
+    'copy:prod',
+    'uglify:prod'
+  ]);
 };
